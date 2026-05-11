9436fddb122fc3993d4bbec38e48f770afdd5935
Chris Jones <chrisj96@proton.me>
2026-05-11 20:26:25 +0100

    Add CarsAssemble.java to exercism-java

---
+++ src/main/java/carsassemble/CarsAssemble.java
@@ -0,0 +1,24 @@
+package carsassemble;
+
+public class CarsAssemble {
+
+    public static double productionRatePerHour(int speed) {
+        int carsPerHour = 221 * speed;
+
+        if (speed >= 1 && speed <=4) {
+            return carsPerHour * 1.0;
+        } else if (speed >= 5 && speed <= 8) {
+            return carsPerHour * 0.9;
+        } else if (speed == 9) {
+            return carsPerHour * 0.8;
+        } else if (speed == 10) {
+            return carsPerHour * 0.77;
+        }
+
+        return 0;
+    }
+
+    public int workingItemsPerMinute(int speed) {
+        throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+    }
+}
