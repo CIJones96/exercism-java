2a8d5ab7928ee77995c2daef0f60bbd24de20edb
Chris Jones <chrisj96@proton.me>
2026-05-12 21:25:23 +0100

    Add BirdWatcher and Unit Tests

---
+++ src/main/java/birdwatcher/BirdWatcher.java
@@ -0,0 +1,38 @@
+package birdwatcher;
+
+class BirdWatcher {
+    private final int[] birdsPerDay;
+
+    public BirdWatcher(int[] birdsPerDay) {
+        this.birdsPerDay = birdsPerDay.clone();
+    }
+
+    public int[] getLastWeek() {
+        return new int[]{0,2,5,3,7,8,4};
+    }
+
+    public int getToday() {
+        return birdsPerDay[birdsPerDay.length - 1];
+    }
+
+    public void incrementTodaysCount() {
+        birdsPerDay[birdsPerDay.length - 1]++;
+    }
+
+    public boolean hasDayWithoutBirds() {
+        for (int birds : birdsPerDay) {
+            if (birds == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getCountForFirstDays(int numberOfDays) {
+        throw new UnsupportedOperationException("Please implement the BirdWatcher.getCountForFirstDays() method");
+    }
+
+    public int getBusyDays() {
+        throw new UnsupportedOperationException("Please implement the BirdWatcher.getBusyDays() method");
+    }
+}
